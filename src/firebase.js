f07c8a8029e23022fbd8994e634e115685f8ef3f
toshcanito <daniel.regx@gmail.com>
2021-02-12 14:24:51 -0700

    avances

--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,13 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  //firebase configuration
+  apiKey: "AIzaSyBHnP76mMfn3DjFl_dPstjNbIUwz1fpRfg",
+  authDomain: "react-crud-e83da.firebaseapp.com",
+  databaseURL: "https://react-crud-e83da.firebaseio.com",
+  projectId: "react-crud-e83da",
+  storageBucket: "react-crud-e83da.appspot.com",
+  messagingSenderId: "330743876718",
+  appId: "1:330743876718:web:421755b0a5d8d2b34d6a7f"
 };
 // Initialize Firebase
 var fireDb = firebase.initializeApp(firebaseConfig);
