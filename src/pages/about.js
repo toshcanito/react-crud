7941dad72644bd9663b02fe66b07f309f2f6bae9
toshcanito <daniel.regx@gmail.com>
2021-02-12 14:26:55 -0700

    avances

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+                <h1 className="display-4 text-center">About</h1>
+            </div>
+        </div>
+    );
+}
+
+export default About;
